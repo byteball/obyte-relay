55b3a59336c00fd122d4e23146c70f2d9fc04e87
tony <tonyofbyteball@users.noreply.github.com>
2016-08-25 10:21:40 +0300

    tabs

--- start.js
+++ start.js
@@ -4,17 +4,17 @@ var conf = require('byteballcore/conf.js');
 var myWitnesses = require('byteballcore/my_witnesses.js');
 
 function start(){
-    console.log('starting');
-    var network = require('byteballcore/network.js');
-    if (conf.initial_peers)
-        conf.initial_peers.forEach(function(url){
-            network.findOutboundPeerOrConnect(url);
-        });
+	console.log('starting');
+	var network = require('byteballcore/network.js');
+	if (conf.initial_peers)
+		conf.initial_peers.forEach(function(url){
+			network.findOutboundPeerOrConnect(url);
+		});
 }
 
 myWitnesses.readMyWitnesses(function(arrWitnesses){
-    if (arrWitnesses.length > 0)
-        return start();
-    console.log('will init witnesses', conf.initial_witnesses);
-    myWitnesses.insertWitnesses(conf.initial_witnesses, start);
+	if (arrWitnesses.length > 0)
+		return start();
+	console.log('will init witnesses', conf.initial_witnesses);
+	myWitnesses.insertWitnesses(conf.initial_witnesses, start);
 }, 'ignore');
