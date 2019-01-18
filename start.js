f6f8516504ae228863c15fbd7f3a457adc61012f
tony <tonyofbyteball@users.noreply.github.com>
2019-01-19 02:23:33 +0300

    obyte

--- start.js
+++ start.js
@@ -1,5 +1,14 @@
 /*jslint node: true */
 "use strict";
+var fs = require('fs');
+var desktopApp = require('ocore/desktop_app.js');
+var appDataDir = desktopApp.getAppDataDir();
+var path = require('path');
+
+if (require.main === module && !fs.existsSync(appDataDir) && fs.existsSync(path.dirname(appDataDir)+'/byteball-relay')){
+	console.log('=== will rename old relay data dir');
+	fs.renameSync(path.dirname(appDataDir)+'/byteball-relay', appDataDir);
+}
 var conf = require('ocore/conf.js');
 var myWitnesses = require('ocore/my_witnesses.js');
 
